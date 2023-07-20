cdedc5542df20531f06a046fb0f7f25e06f7787e
Alan Christie <alan.christie@matildapeak.com>
2023-07-20 09:46:09 +0100

    build: Switch to decoder 1.17.0 and PyYAML 6.0.1

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setuptools.setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: System :: Installation/Setup",
         "Operating System :: POSIX :: Linux",
     ],
